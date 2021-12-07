f8c35d9565540c9620f79ebf44341e6e3d198742
Brian Moniz <bmoniz328@gmail.com>
2021-12-07 14:50:22 -0800

    began server and client folders

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
+const cors = require('cors')
 
-require('./routes/person.routes')(app); 
+app.use(cors());
+// cors allows the ability to make cross-origin requests
+// for example: being on localhost:3000 but making a
+// request to localhost:8000. 
+
+require('./routes/person.routes')(app);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`) )
